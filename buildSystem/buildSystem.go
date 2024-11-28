15ae31ff73d8b5c5aa63491d8024c432f57e1c17
Logan <89676793+SpiralUltimate@users.noreply.github.com>
2024-11-27 21:56:48 -0800

    Initial commit

---
+++ buildSystem/buildSystem.go
@@ -0,0 +1,53 @@
+package buildSystem
+
+import "os"
+
+// Instantiate a builder class
+type Builder struct {
+	// Project name
+	project string
+	// Compiler
+	compiler string
+	// Project files
+	projectFiles []string
+	// Cmake file being edited
+	cmakeFile os.File
+}
+
+// Parses Builder struct into cmake file
+
+func (build *Builder) Parse() error {
+	// Create a new CMake file
+	cmakeFile, err := os.Create("CMakeLists.txt")
+	// Error check cmake file
+	if err != nil {
+		return err
+	}
+	// Close cmake file at end of function
+	defer cmakeFile.Close()
+
+	// Write project name and compiler info
+	cmakeFile.WriteString("cmake_minimum_required(VERSION 3.16)\n")
+	cmakeFile.WriteString("project(" + build.project + ")\n")
+	cmakeFile.WriteString("set(CMAKE_CXX_STANDARD 17)\n")
+	cmakeFile.WriteString("set(CMAKE_CXX_COMPILER " + build.compiler + ")\n\n")
+
+	// Write project files
+	for _, file := range build.projectFiles {
+		cmakeFile.WriteString("add_executable(" + build.project + " " + file + ")\n")
+	}
+
+	// Return nil as error to indicate success
+	return nil
+}
+
+// Makes a new project, using the given name
+func (build *Builder) Project(name string, compiler string) {
+	build.project = name
+	build.compiler = compiler
+}
+
+// Adds files to the project
+func (build *Builder) AddFiles(files ...string) {
+	build.projectFiles = append(build.projectFiles, files...)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/GoLionBuildSystem
+
+go 1.21.6
